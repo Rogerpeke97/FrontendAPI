25a007948167f125fde5ae3ff04bbce86b0b5b2d
Ignacio <rogerpeke97@gmail.com>
2021-01-07 16:32:11 -0300

    Added jumping animation, button events and functionality finished

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -24,9 +24,10 @@ const Game = () => {
     let obj = useRef(0);
     let switcher = useRef(0);
     let mixer = useRef(0);
-    let keydownStoppedLeft = useRef(false);
-    let keydownStoppedRight = useRef(false);
-    let trackedKeys = useRef(0)
+    let isRotating = useRef(0)
+    let trackedKeys = useRef(0);
+    let centerChar = useRef(false);
+    let jumpChar = useRef(false);
     useEffect(() => {
         if (scene === "Scene not set") {
             let height = canvas.current.clientHeight
@@ -93,17 +94,36 @@ const Game = () => {
                     .set(0.3, 0.3, 0.3)
                 obj.current = object;
                 mixer.current = new THREE.AnimationMixer(obj.current.scene);
+
                 let action = mixer
                     .current
                     .clipAction(obj.current.animations[15]) // IDLE ANIMATION
                 action.play()
                 action.clampWhenFinished = true;
-                /*  mixer
-                    .clipAction(object.animations[5])// WALKING ANIMATION
-                    .play();*/
+
                 scene.add(object.scene);
                 switcher.current = 1
+                setTimeout(()=>{
+                    let action = mixer
+                       .current
+                       .clipAction(obj.current.animations[14]) // RUN ANIMATION
+                    action.play()
+                    action.clampWhenFinished = true; 
+                    console.log(scene.children);
+                },10000)
+                mixer
+                .current
+                .clipAction(obj.current.animations[0]); // JUMP ANIMATION
+            //EVENT LISTENER FOR FINISHED ANIMATION
+            mixer.current.addEventListener('loop', (e)=>{
+                if(e.action._clip.name === "knight_jump_up_root"){
+                    let action = mixer.current._actions[2];
+                    action.stop();
+                    jumpChar.current = false;
+                }
+            })
             },);
+
 
             //SETTING THE ARROW EVENTS ARROW RIGHT MOVE RIGHT
             // STORING THE KEY VALUES IN TRACKEDKEYS DUE TO DELAY
@@ -112,54 +132,75 @@ const Game = () => {
             //SEE https://yojimbo87.github.io/2012/08/23/repeated-and-multiple-key-press-events-without-stuttering-in-javascript.html for full understanding of delay.
             //Repeated or multiple key press events in JavaScript can cause a little pause or delay which leads to stuttering behavior, 
             //for example, in games which are using keyboard based navigation. This delay is probably caused by internal browser timeout between key press changes.
+            //TRACKING KEYS
             trackedKeys.current = {
                 arrowLeft: false, // left arrow
                 arrowRight: false, // right arrow
-            };
+                rightMove: function(){ // ()=>{} THIS DOESNT WORK INSIDE OBJECTS :/
+                    this.arrowLeft = false;
+                    this.arrowRight = true;
+                },
+                leftMove: function(){
+                    this.arrowLeft = true;
+                    this.arrowRight = false;         
+                }
+            }
+            //IS ROTATING TRACKER 
+            isRotating.current = {
+                isRotatingRight : "stop",
+                isRotatingLeft : "stop",
+                rotateRight: function(){
+                    this.isRotatingRight = "start";
+                    this.isRotatingLeft = "stop";
+                },
+                rotateLeft: function(){
+                    this.isRotatingRight = "stop";
+                    this.isRotatingLeft = "start";
+                },
+            }
+
 
             window.addEventListener('keydown', (e) => {
                 if (e.key === "ArrowRight" && trackedKeys.current["arrowRight"] === false) {
-                    keydownStoppedRight.current = false;
-                    keydownStoppedLeft.current = true;
-                    trackedKeys.current.arrowRight = true;
-                    trackedKeys.current.arrowLeft = false;
+                    trackedKeys.current.rightMove();
+                    isRotating.current.rotateRight();
                 }
                 else if (e.key === "ArrowLeft" && trackedKeys.current["arrowLeft"] === false){
-                    keydownStoppedLeft.current = false;
-                    keydownStoppedRight.current = true;
-                    trackedKeys.current.arrowRight = false;
-                    trackedKeys.current.arrowLeft = true;
+                    trackedKeys.current.leftMove();
+                    isRotating.current.rotateLeft();
                 }
+                else if (e.key === " " && jumpChar.current === false){
+                    console.log("spacebar pressed")
+                    jumpChar.current = true;
+                }
+
             })
             window.addEventListener('keyup', (e) => {
                 if (e.key === "ArrowRight") {
-                    keydownStoppedRight.current = true;
                     trackedKeys.current.arrowRight = false;
                 }
-                if (e.key === "ArrowLeft") {
-                    keydownStoppedLeft.current = true;
+                else if (e.key === "ArrowLeft") {
                     trackedKeys.current.arrowLeft = false;
                 }
             })
-            //MOVE LEFT
 
+
+            //MOVING 
             let charRotateAndMove = () => {
-                console.log(JSON.stringify(trackedKeys.current));
                     switch (JSON.stringify(trackedKeys.current)) {
-                    case `{"arrowLeft":true,"arrowRight":false}`:
-                        console.log("ROTATING LEFT")
+                    case `{"arrowLeft":true,"arrowRight":false}`: //MOVE LEFT
+                        centerChar.current = false;
+                        console.log("MOVING LEFT")
                         let tween2 = new TWEEN
                             .Tween(obj.current.scene.position)
                             .to({
-                                x: obj.current.scene.position.x - 0.5,
+                                x: obj.current.scene.position.x - 0.7,
                                 y: 0,
                                 z: 4
                             })
-                            .onComplete(()=>{
-                            })
                             .start()
                             let animateTween2 = (time) => {
-                            if (keydownStoppedLeft.current === true) {     
+                            if (trackedKeys.current.arrowLeft === false) {     
                                 TWEEN.remove(tween2);
                             } else {
                                 TWEEN.update(time)
@@ -167,21 +208,46 @@ const Game = () => {
                             }
                         }
                         requestAnimationFrame(animateTween2);
+                        //ROTATION
+                        if(isRotating.current.isRotatingLeft === "start"){
+                        let tweenRotateLeft = new TWEEN
+                        .Tween(obj.current.scene.rotation)
+                        .to({
+                            x: 0,
+                            y: -2.5, // FROM -3 TO -2.5
+                            z: 0
+                        }, 50)
+                        .onComplete(()=>{
+                                TWEEN.remove(tweenRotateLeft);
+                                isRotating.current.rotateRight();
+                                centerChar.current = true;
+                        })
+                        .start()
+                        let animateTweenRotateLeft = (time) => {
+                        if (trackedKeys.current.arrowLeft === false){     
+                            TWEEN.remove(tweenRotateLeft);
+                            centerChar.current = true;
+                        } else {
+                            TWEEN.update(time)
+                            requestAnimationFrame(animateTweenRotateLeft)
+                        }
+                    }
+                    requestAnimationFrame(animateTweenRotateLeft);
+                }
                         break;
-                    case `{"arrowLeft":false,"arrowRight":true}`:
-                        console.log("ROTATING RIGHT");
+                    case `{"arrowLeft":false,"arrowRight":true}`: // MOVE RIGHT
+                        console.log("MOVING RIGHT");
+                        centerChar.current = false;
                         let tween4 = new TWEEN
                             .Tween(obj.current.scene.position)
                             .to({
-                                x: obj.current.scene.position.x + 0.5,
+                                x: obj.current.scene.position.x + 0.7,
                                 y: 0,
                                 z: 4
                             })
-                            .onComplete(()=>{
-                            })
                             .start()
                             let animateTween4 = (time) => {
-                            if (keydownStoppedRight.current === true) {
+                            if (trackedKeys.current.arrowRight === false) {
                                 TWEEN.remove(tween4);
                             } else {
                                 TWEEN.update(time)
@@ -189,13 +255,74 @@ const Game = () => {
                             }
                         }
                         requestAnimationFrame(animateTween4)
+                        //ROTATION
+                        if(isRotating.current.isRotatingRight === "start"){
+                        let tweenRotateRight = new TWEEN
+                        .Tween(obj.current.scene.rotation)
+                        .to({
+                            x: 0,
+                            y: -3.5, // FROM -3 TO -3.5
+                            z: 0
+                        }, 50)
+                        .onComplete(()=>{
+                                TWEEN.remove(tweenRotateRight);
+                                isRotating.current.rotateLeft();
+                                centerChar.current = true;
+                        })
+                        .start()
+                        let animateTweenRotateRight = (time) => {
+                        if (trackedKeys.current.arrowRight === false) {     
+                            TWEEN.remove(tweenRotateRight);
+                            centerChar.current = true;
+                        } else {
+                            TWEEN.update(time)
+                            requestAnimationFrame(animateTweenRotateRight)
+                        }
+                    }
+                    requestAnimationFrame(animateTweenRotateRight);        
+                }            
                         break;
                     default:
                 }
             }
             setInterval(()=>charRotateAndMove(), 50);
+            //ROTATIOn CENTER CHARACTER
+            let centerCharFunction = ()=>{
+                if(centerChar.current === true){
+                let tweenRotateCenter = new TWEEN
+                        .Tween(obj.current.scene.rotation)
+                        .to({
+                            x: 0,
+                            y: -3, // FROM -3 TO -3
+                            z: 0
+                        }, 50)
+                        .onComplete(()=>{
+                            TWEEN.remove(tweenRotateCenter)
+                            centerChar.current = false;
+                            })
+                        .start()
+                        let animateTweenRotateCenter = (time) => {
+                            TWEEN.update(time)
+                            requestAnimationFrame(animateTweenRotateCenter)
+                    }
+                    requestAnimationFrame(animateTweenRotateCenter);
+                }        
+            }
+            setInterval(()=>centerCharFunction(), 50);
+
+            //JUMP ANIMATION
+
+            let jump = ()=>{
+                if(jumpChar.current === true && mixer.current._actions[2] !== undefined){
+                    let action = mixer.current._actions[2] // JUMP ANIMATION
+                    action.play();
+                }      
+            }
+            setInterval(()=>jump(), 50);
+
+
                 //PLANE
-                for (let i = 4; i > -1; i -= 0.5) {
+                for (let i = 6; i > -1; i -= 0.5) {
                     for (let j = -2; j < 2; j += 0.5) {
                         let floorTexture = new THREE
                             .TextureLoader()
